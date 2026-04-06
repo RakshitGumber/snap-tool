b180e440c1e729e4fa66e2d0b935a6eb4b2610f5
RakshitGumber <gumberrakshit@gmail.com>
2026-04-06 23:00:49 +0530

    removed Elysia HMR

--- public/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import '@public/styles/global.css'
-
-function App() {
-	return <div>Hello</div>
-}
-
-const root = createRoot(document.getElementById('root')!)
-root.render(<App />)

a6dc622a8e18ea092694b8edf4a7b1ed7b7810b7
RakshitGumber <gumberrakshit@gmail.com>
2026-04-13 23:42:14 +0530

    changed font color for heding in the navbar

--- web/src/Components/main/Navbar.tsx
+++ web/src/Components/main/Navbar.tsx
@@ -9,13 +9,13 @@ export const Navbar = () => {
   const { session } = useAuthStore();
 
   return (
-    <header className="bg-card-bg border-b-2 border-accent dark:border-accent/70 w-full fixed top-0 z-20 flex justify-center">
+    <header className="h-18 bg-card-bg border-b-2 border-accent dark:border-accent/70 w-full fixed top-0 z-20 flex justify-center">
       <nav className="max-w-7xl flex items-center justify-between p-3 flex-1">
         <Link
-          className="hover:bg-text-color/20 rounded-lg flex items-center py-2 px-4 cursor-pointer"
+          className="hover:bg-text-color/30 rounded-lg flex items-center py-2 px-4 cursor-pointer"
           to="/"
         >
-          <h1 className="font-heading text-xl text-title-color font-semibold tracking-wide uppercase select-none">
+          <h1 className="font-heading text-2xl text-accent  font-medium tracking-widest capitalize select-none">
             Single Filter
           </h1>
         </Link>

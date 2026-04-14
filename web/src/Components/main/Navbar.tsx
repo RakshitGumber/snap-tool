83746b0f2cb3ff4948c9ea39272d6f58c721b9ef
RakshitGumber <gumberrakshit@gmail.com>
2026-04-14 17:07:19 +0530

    added logout button and the profile button

--- web/src/Components/main/Navbar.tsx
+++ web/src/Components/main/Navbar.tsx
@@ -4,9 +4,17 @@ import { useAuthStore } from "@/stores/useAuthStore";
 import { Icon } from "@iconify/react";
 
 import { ThemeButton } from "../ui/ThemeButton";
+import { useEffect, useState } from "react";
 
 export const Navbar = () => {
-  const { session } = useAuthStore();
+  const { session, signOut } = useAuthStore();
+  const [showProfile, setShowProfile] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => setShowProfile(false));
+
+    removeEventListener("scroll", () => setShowProfile(false));
+  }, [showProfile]);
 
   return (
     <header className="h-18 bg-card-bg border-b-2 border-accent dark:border-accent/70 w-full fixed top-0 z-20 flex justify-center">
@@ -16,7 +24,7 @@ export const Navbar = () => {
             Single Filter
           </h1>
         </Link>
-        <ul className="flex gap-4">
+        <ul className="flex gap-1">
           <li className="flex px-4 py-2 hover:bg-text-color/20 cursor-pointer rounded-lg">
             <Link to="/">Home</Link>
           </li>
@@ -27,27 +35,42 @@ export const Navbar = () => {
             <Link to="/contribute">Contribute</Link>
           </li>
         </ul>
-        <div className="relative flex items-center gap-3">
+        <div className="flex items-center gap-3">
           <div className="flex gap-2">
             <a
               href="https://github.com/RakshitGumber/snap-tool"
               className="rounded-lg font-semibold hover:bg-accent-light items-center flex gap-1 text-xl p-2 text-title-color"
+              target="_blank"
             >
               <Icon icon="simple-icons:github" />
             </a>
             <ThemeButton />
           </div>
           {session?.user ? (
-            <Link
-              to="/create"
-              className="rounded-lg font-semibold hover:bg-accent-light items-center flex gap-1 text-xl px-3 py-1 hover:text-title-color"
-            >
-              <span className="text-lg text-title-color">Create</span>
-              <Icon
-                icon="ooui:link-external-ltr"
-                className="text-accent mb-0.5"
-              />
-            </Link>
+            <>
+              <button
+                className="rounded-full cursor-pointer"
+                onClick={() => setShowProfile(true)}
+              >
+                <img
+                  src={
+                    session.user.image ??
+                    `https://ui-avatars.com/api/?name=${session.user.name}&background=37af87&font-size=0.4&bold=true&color=fff`
+                  }
+                  className="rounded-full w-8"
+                />
+              </button>
+              <Link
+                to="/create"
+                className="rounded-lg font-semibold hover:bg-accent-light items-center flex gap-1 text-xl px-3 py-1 hover:text-title-color"
+              >
+                <span className="text-lg text-title-color">Create</span>
+                <Icon
+                  icon="ooui:link-external-ltr"
+                  className="text-accent mb-0.5"
+                />
+              </Link>
+            </>
           ) : (
             <Link
               to="/auth/register"
@@ -56,6 +79,16 @@ export const Navbar = () => {
               Register
               <Icon icon="solar:add-square-broken" className="text-xl" />
             </Link>
+          )}
+          {showProfile && (
+            <div className="absolute top-18 rounded-b-lg border-b-2 border-accent right-30 w-50 bg-card-bg p-4">
+              <button
+                className="w-full bg-accent py-1 text-lg rounded-lg text-bg font-semibold"
+                onClick={signOut}
+              >
+                Log Out
+              </button>
+            </div>
           )}
         </div>
       </nav>

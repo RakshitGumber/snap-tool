a8573f651be8e1f31daba583aea1cfa7b25d428b
RakshitGumber <gumberrakshit@gmail.com>
2026-04-14 23:20:40 +0530

    making anomation objects

--- web/src/Components/ui/HeroAnimation.tsx
+++ web/src/Components/ui/HeroAnimation.tsx
@@ -1,9 +1,24 @@
+import { motion } from "framer-motion";
+
 export const HeroAnimation = () => {
   return (
-    <div className="relative flex flex-col flex-1 items-center justify-end py-32">
-      <div className="border-16 border-b-60 border-white shadow-[6px_6px_15px_#0008] -rotate-6 select-none">
-        <div className="h-80 w-80 bg-white"></div>
-      </div>
+    <div className="relative flex flex-1 items-center justify-center">
+      <motion.div className="relative border-16 border-white border-b-80 -rotate-6">
+        <motion.div
+          initial={{ background: "white" }}
+          animate={{
+            background:
+              "linear-gradient(180deg, rgba(4, 71, 54, 1) 0%, rgba(75, 173, 135, 1) 68%, rgba(104, 220, 152, 1) 100%)",
+          }}
+          className="w-100 h-100 z-1"
+        ></motion.div>
+        <motion.div className="">
+          <img
+            src="/images/ferret.png"
+            className="absolute top-20 drop-shadow-2xl drop-shadow-teal-200 z-2"
+          />
+        </motion.div>
+      </motion.div>
     </div>
   );
 };

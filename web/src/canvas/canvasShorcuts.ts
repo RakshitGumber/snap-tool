5aad97995d9a0af427e658eeee19a5abafe12a63
RakshitGumber <gumberrakshit@gmail.com>
2026-04-19 10:23:35 +0530

    Removed Canvas Movement shortcuts

--- web/src/canvas/canvasShorcuts.ts
+++
@@ -1,8 +0,0 @@
-import type { CanvasActions, ShortcutMap } from "@/types/canvas";
-
-export const CanvasShortcuts = (actions: CanvasActions): ShortcutMap => ({
-  delete: actions.delete,
-  backspace: actions.delete,
-  "ctrl+s": actions.save,
-  "ctrl+x": actions.clear,
-});

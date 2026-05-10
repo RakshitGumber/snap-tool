9ea220d58e16c49e5acbd781bdb8d52a18d4c453
RakshitGumber <gumberrakshit@gmail.com>
2026-05-10 17:09:50 +0530

    removed cloudflare dependency

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react(), cloudflare()],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
